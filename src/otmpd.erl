3bb2d508255306c346cdc8ee391ed7f977243eb9
Bucky Wolfe <bwolfe@franklinamerican.com>
2014-08-21 11:46:03 -0500

    Rename pb defs so modules end in _proto. Add *_proto to gitignore. Update otmpd module to use _proto modules.

--- src/otmpd.erl
+++ src/otmpd.erl
@@ -4,16 +4,16 @@
 handle_msg(Socket, Host, Port, Bin) ->
   io:format("Recv from ~p:~p@~p, ~p~n", [Host, Port, Socket, Bin]),
   %M = otmp_common:encode_msg({'Meta', "Connect", <<"1","2","3">>}).                            
-  { _, MsgName, Payload } = otmp_common:decode_msg(Bin, 'Meta'),
+  { _, MsgName, Payload } = otmp_common_proto:decode_msg(Bin, 'Meta'),
   case MsgName of
     "Connect"    -> handle_connect(Payload);
     "Disconnect" -> handle_disconnect(Payload)
   end.
 
 handle_disconnect(Payload) ->
-  P = otmp_client:decode_msg(Payload, 'Disconnect'),
+  P = otmp_client_proto:decode_msg(Payload, 'Disconnect'),
   io:format("Recv DISCONNECT -> ~p", [P]).
 
 handle_connect(Payload) ->
-  P = otmp_client:decode_msg(Payload, 'Connect'),
+  P = otmp_client_proto:decode_msg(Payload, 'Connect'),
   io:format("Recv CONNECT -> ~p", [P]).
