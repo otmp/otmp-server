7175cd401e52f96a29b376c5b5257628a88a21fa
Bucky Wolfe <bwolfe@franklinamerican.com>
2014-08-21 03:57:49 -0500

    New msg type, parse meta, handle connect and disconnect

--- src/otmpd.erl
+++ src/otmpd.erl
@@ -3,4 +3,17 @@
 
 handle_msg(Socket, Host, Port, Bin) ->
   io:format("Recv from ~p:~p@~p, ~p~n", [Host, Port, Socket, Bin]),
-  gen_udp:send(Socket, Host, Port, Bin).
+  %M = otmp_common:encode_msg({'Meta', "Connect", <<"1","2","3">>}).                            
+  { _, MsgName, Payload } = otmp_common:decode_msg(Bin, 'Meta'),
+  case MsgName of
+    "Connect"    -> handle_connect(Payload);
+    "Disconnect" -> handle_disconnect(Payload)
+  end.
+
+handle_disconnect(Payload) ->
+  P = otmp_client:decode_msg(Payload, 'Disconnect'),
+  io:format("Recv DISCONNECT -> ~p", [P]).
+
+handle_connect(Payload) ->
+  P = otmp_client:decode_msg(Payload, 'Connect'),
+  io:format("Recv CONNECT -> ~p", [P]).
