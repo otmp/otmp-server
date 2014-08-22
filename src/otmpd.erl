adaca623a723e9851f41ccf80c5a05ff5c93bad6
Bucky Wolfe <bwolfe@franklinamerican.com>
2014-08-21 20:51:13 -0500

    remove otmp_common

--- src/otmpd.erl
+++ src/otmpd.erl
@@ -9,11 +9,6 @@ handler() ->
       handler()
   end.
 
-handle(connect, Payload, _Hostdata) ->
-  P = otmp_client_proto:decode_msg(Payload, connect),
-  io:format("Recv CONNECT -> ~p~n", [P]);
-
-handle(disconnect, Payload, _Hostdata) ->
-  P = otmp_client_proto:decode_msg(Payload, disconnect),
-  io:format("Recv DISCONNECT -> ~p~n", [P]).
-
+handle(MsgType, Payload, _Hostdata) ->
+  P = otmp_client_proto:decode_msg(Payload, MsgType),
+  io:format("Recv ~p -> ~p~n", [MsgType, P]).
